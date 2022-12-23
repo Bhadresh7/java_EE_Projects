057b61db9915f0b1b34fea456ecff1ef7d4bdc56
Bhadresh7 <bhadreshpalani19@gmail.com>
2022-12-23 14:28:13 +0530

    addition program

---
+++ EE_project/src/testing1/addition.java
@@ -0,0 +1,17 @@
+package testing1;
+import java.util.*;
+
+class addition
+{
+	public static void main(String args[])
+			{
+				int a ,b,c;
+				Scanner s=new Scanner(System.in);
+				System.out.println("Enter the first value:");
+				a=s.nextInt();
+				System.out.println("Enter the second value:");
+				b=s.nextInt();
+				c=a+b;
+				System.out.println("result:"+c);
+			}
+}
