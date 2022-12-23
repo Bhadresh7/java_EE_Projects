3805b2ca1b5fa45fa83760c17f1e6ceebb1509bd
Bhadresh7 <bhadreshpalani19@gmail.com>
2022-12-23 13:41:54 +0530

    hi

--- EE_project/src/testing1/addition.java
+++ EE_project/src/testing1/addition.java
@@ -1,18 +1,18 @@
 package testing1;
 import java.util.*;
-public class addition 
-{
 
-	public static void main(String[] args)
-	{
+public class addition {
+
+	public static void main(String[] args) {
 		int a,b,c;
-		System.out.println("Enter the first value:");
 		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the first value:");
 		a=s.nextInt();
 		System.out.println("Enter the second value:");
 		b=s.nextInt();
 		c=a+b;
-		System.out.println("sum of two values:"+c);
+		System.out.println("result:"+c);
+
 	}
 
 }
