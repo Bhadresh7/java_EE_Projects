da5f0d60514309311903f734268357beeb7ae531
Bhadresh7 <bhadreshpalani19@gmail.com>
2022-12-24 00:54:14 +0530

    subtraction program in java

---
+++ subtraction/src/subtraction_java/subtract.java
@@ -0,0 +1,17 @@
+package subtraction_java;
+import java.util.*;
+public class subtract {
+
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the first value:");
+		a=s.nextInt();
+		System.out.println("Enter the second value:");
+		b=s.nextInt();
+		c=a-b;
+		System.out.println("result:");
+	}
+
+}
